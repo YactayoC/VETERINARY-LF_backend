fc465ad19624438cb577c4def5bac2ba09f0a871
Yactayo <syactayocrisostomo@gmail.com>
2022-05-05 14:25:57 -0500

    make it better

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const { dbConnection } = require('./database/config');
 const app = express();
 dbConnection();
 app.use(cors());
+app.use(express.static('public'));
 app.use(express.json());
 
 app.use('/api/auth', require('./routes/auth'));
